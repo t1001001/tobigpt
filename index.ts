cbbfdb6715ee490811ff19815ac78968bd530732
tobi <tobias1001001@proton.me>
2026-08-09 20:08:13 +0000

    updated index.ts and added express types as dev dependency

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-export function main() {
-    console.log("Hello World!")
-}
+import express from "express";
+const app = express();
+const port = "3000";
 
-main()
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("Response sent");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
